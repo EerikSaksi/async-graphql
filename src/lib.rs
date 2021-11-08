53eb91590a61ad9467833ea20860d50381d1664e
Eerik Saksi <saksi.eerik@gmail.com>
2021-11-08 19:57:18 +0000

    Now exports check_rules

--- src/lib.rs
+++ src/lib.rs
@@ -219,7 +219,7 @@ pub use request::{BatchRequest, Request};
 pub use resolver_utils::{ContainerType, EnumType, ScalarType};
 pub use response::{BatchResponse, Response};
 pub use schema::{Schema, SchemaBuilder, SchemaEnv};
-pub use validation::{ValidationMode, ValidationResult, VisitorContext};
+pub use validation::{ValidationMode, ValidationResult, VisitorContext, check_rules};
 
 pub use context::*;
 #[doc(no_inline)]

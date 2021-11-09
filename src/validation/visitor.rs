13858ffafc3691126af6aa427b998c0f2c333cec
Eerik Saksi <saksi.eerik@gmail.com>
2021-11-09 11:31:21 +0000

    Debugging

--- src/validation/visitor.rs
+++ src/validation/visitor.rs
@@ -557,6 +557,7 @@ fn visit_operation_definition<'a, V: Visitor<'a>>(
     operation: &'a Positioned<OperationDefinition>,
 ) {
     v.enter_operation_definition(ctx, name, operation);
+    println!("{:?}", operation);
     let root_name = match &operation.node.ty {
         OperationType::Query => Some(&*ctx.registry.query_type),
         OperationType::Mutation => ctx.registry.mutation_type.as_deref(),
@@ -564,7 +565,7 @@ fn visit_operation_definition<'a, V: Visitor<'a>>(
     };
     if let Some(root_name) = root_name {
         println!("root_name {}", root_name);
-        println!("root_name {:?}", ctx.registry.types.keys());
+        println!("{:?}", ctx.registry.types.keys());
         ctx.with_type(Some(&ctx.registry.types[&*root_name]), |ctx| {
             visit_variable_definitions(v, ctx, &operation.node.variable_definitions);
             visit_directives(v, ctx, &operation.node.directives);
